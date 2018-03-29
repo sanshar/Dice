b6bc5d9ab2f45bfb8a8ebda5aec323956d0a3c06
xubwa <xubo.wang@colorado.edu>
2018-03-29 15:48:53 -0600

    still with minor error

--- global.h
+++ global.h
@@ -31,7 +31,6 @@ const int DetLen = 6;
 extern double startofCalc;
 double getTime();
 
-#define Complex //Artificially add this for 4c test
 #ifdef Complex
 #define MatrixXx MatrixXcd
 #define CItype std::complex<double>
