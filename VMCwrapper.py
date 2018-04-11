fdad301c33e38532f76dd5cb17a570ba52189a46
Sandeep Sharma <sash2458@login01.rc.colorado.edu>
2018-04-11 15:45:35 -0600

    backups the parameters with the best energy. Also restart is working. The keyword restart should be placed in vmc.dat

--- VMCwrapper.py
+++ VMCwrapper.py
@@ -3,7 +3,7 @@ import climin
 import sys
 import climin.rmsprop
 import climin.amsgrad
-from subprocess import check_call, CalledProcessError
+from subprocess import check_output, check_call, CalledProcessError
 
 def getopts(argv):
     opts = {}  # Empty dictionary to store key-value pairs.
@@ -26,40 +26,65 @@ if '-n' in myargs:
 
 f = open(inFile, 'r')
 correlatorSize, numCorrelators = 0, 0
+Restart = False
 for line in f:
     linesp = line.split();
     if (len(linesp) != 0 and linesp[0][0] != "#" and linesp[0] == "correlator"):
         correlatorFile = linesp[2]
         correlatorSize = int(linesp[1])
-	print linesp, correlatorFile
+	#print linesp, correlatorFile
         f2 = open(correlatorFile, 'r')
         for line2 in f2:
             if (line2.strip(' \n') != ''):
                 numCorrelators += 1
-        break;
-
+    elif ( len(linesp) != 0 and linesp[0][0] != "#" and linesp[0].lower() == "restart"):
+	Restart = True
+    
 numVars = numCorrelators*2**(2*correlatorSize)
-print numVars, numCorrelators, correlatorSize
+emin = 1.e10
+
 def d_loss_wrt_pars(wrt):
+    global emin
     wrt.astype('float64').tofile("params.bin")
     try:
         cmd = ' '.join((mpiprefix, executable, inFile))
         cmd = "%s " % (cmd)
-        check_call(cmd, shell=True)
+        #check_call(cmd, shell=True)
+        out=check_output(cmd, shell=True).strip()
+        print out
+        sys.stdout.flush()
+        e=float(out.split()[0])
     except CalledProcessError as err:
         raise err
+
+    if e<emin:
+        emin = 1.*e
+        wrt.astype('float64').tofile("params_min.bin")
+        eminA = np.asarray([emin])
+        eminA.astype('float64').tofile("emin.bin")
+
     p = np.fromfile("grad.bin", dtype="float64")
     p.reshape(wrt.shape)
     return p
 
+
 wrt = np.ones(shape=(numVars,))
+if (Restart):
+    wrt  = np.fromfile("params.bin", dtype = "float64")
+    emin = np.fromfile("emin.bin", dtype = "float64")[0]
+
 #wrt = np.fromfile("params.bin", dtype="float64")
 #opt = climin.GradientDescent(wrt, d_loss_wrt_pars, step_rate=0.01, momentum=.95)
 #opt = climin.rmsprop.RmsProp(wrt, d_loss_wrt_pars, step_rate=0.0001, decay=0.9)
 opt = climin.amsgrad.Amsgrad(wrt, d_loss_wrt_pars, step_rate=0.001, decay_mom1=0.1, decay_mom2=0.001, momentum=0.0)
 
+if (Restart):	
+   opt.est_mom1_b = np.fromfile("moment1.bin", dtype="float64")
+   opt.est_mom2_b = np.fromfile("moment2.bin", dtype="float64")
+
 for info in opt:
     if info['n_iter'] >= 5000:
-        break
-    
+        break 
+    opt.est_mom1_b.astype("float64").tofile("moment1.bin")
+    opt.est_mom2_b.astype("float64").tofile("moment2.bin")
 
