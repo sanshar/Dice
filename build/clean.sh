bb415a4e494692f7665b9b1931aaa60bd5a40f65
James Smith <james.smith9113@gmail.com>
2018-08-27 11:58:19 -0600

    Working unit-test with cmake.

---
+++ build/clean.sh
@@ -0,0 +1,1 @@
+rm -rf CMake* CTest* unittest/* *tcl *cmake
--- unittests/unit-symmetry.cpp
+++ unittests/unit-symmetry.cpp
@@ -2,9 +2,9 @@
 
 // Let Catch provide main():
 #define CATCH_CONFIG_MAIN
+#include "../Dice/symmetry.h"
 #include <catch.hpp>
 #include <string>
-#include "../Dice/symmetry.h"
 
 TEST_CASE("All products of the same irrep should be Ag (1)", "[symmetry]") {
   symmetry sym((std::string) "d2h");
@@ -20,4 +20,10 @@ mpicxx -std=c++11 -g -O3 -I/Users/jets/apps/Dice/External/Catch
 -L/Users/jets/apps/boost_1_67_0/stage/lib -o unit-symmetry unit-symmetry.cpp
 /Users/jets/apps/Dice/build/CMakeFiles/Dice.dir/Dice/symmetry.cpp.o -lboost_mpi
 -lboost_serialization  && ./unit-symmetry --success
+
+mpicxx -std=c++11 -g -O3 -I/home/jets/apps/test/Dice/External/Catch
+-I/home/jets/apps/boost_1_67_0/ -I/home/jets/apps/eigen
+-L/home/jets/apps/boost_1_67_0/stage/lib -o unit-symmetry unit-symmetry.cpp
+/home/jets/apps/test/Dice/build/CMakeFiles/Dice.dir/Dice/symmetry.cpp.o
+-lboost_mpi -lboost_serialization  && ./unit-symmetry --success
 */
