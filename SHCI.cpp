81ceb1e327b45e8db823244620862738214e7b3b
warlocat <warlocat@zju.edu.cn>
2018-09-11 17:32:18 -0600

    debug

--- SHCI.cpp
+++ SHCI.cpp
@@ -1,20 +1,15 @@
 /*
   Developed by Sandeep Sharma with contributions from James E. T. Smith and Adam
   A. Holmes, 2017 Copyright (c) 2017, Sandeep Sharma
-
   This file is part of DICE.
-
   This program is free software: you can redistribute it and/or modify it under
   the terms of the GNU General Public License as published by the Free Software
   Foundation, either version 3 of the License, or (at your option) any later
   version.
-
   This program is distributed in the hope that it will be useful, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
   FOR A PARTICULAR PURPOSE.
-
   See the GNU General Public License for more details.
-
   You should have received a copy of the GNU General Public License along with
   this program. If not, see <http://www.gnu.org/licenses/>.
 */
@@ -651,13 +646,11 @@ int main(int argc, char* argv[]) {
         SHCIrdm::loadRDM(schd, s2RDMdisk, twoRDMdisk, 0);
         s2RDMdisk = s2RDMdisk + s2RDM.adjoint() + s2RDM;
         SHCIrdm::saveRDM(schd, s2RDMdisk, twoRDMdisk, 0);
-        //SHCIrdm::ComputeEnergyFromSpinRDM(norbs, nelec, I1, I2, coreE, s2RDMdisk);
-        //SHCIrdm::ComputeEnergyFromSpatialRDM(nSpatOrbs, nelec, I1, I2, coreE, s2RDM);
       }
 
-      pout << " response ";
-      SHCIrdm::ComputeEnergyFromSpatialRDM(norbs, nelec, I1, I2, coreE,
-       s2RDM);
+      // pout << " response ";
+      // SHCIrdm::ComputeEnergyFromSpatialRDM(norbs, nelec, I1, I2, coreE,
+      // s2RDM);
     }  // end if doResponse||DoRDM && RdmType && !stochastic...
 
     // #####################################################################
