97ca89355a8de1c7e02fc2833bafe2dca1845b06
Ankit Mahajan <anma2640@shas0137.rc.int.colorado.edu>
2018-09-24 17:08:57 -0600

    added ghf and lanczos tests

--- executables/VMC.cpp
+++ executables/VMC.cpp
@@ -138,7 +138,7 @@ int main(int argc, char *argv[])
       AMSGrad optimizer(schd.stepsize, schd.decay1, schd.decay2, schd.maxIter);
       //functor1 getStochasticGradient = boost::bind(&getGradientWrapper<CIWavefunction<CPSSlater, HFWalker, Operator>, HFWalker>::getGradient, &wrapper, _1, _2, _3, _4, _5, schd.deterministic);
       optimizer.optimize(vars, getStochasticGradient, schd.restart);
-      if (commrank == 0) wave.printVariables();
+      //if (commrank == 0) wave.printVariables();
     }
     else if (schd.method == sgd) {
       SGD optimizer(schd.stepsize, schd.maxIter);
