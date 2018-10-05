c6c0c03a81b0a1d049743f034d69b475c4f37cdc
jamesETsmith <jasm3285@colorado.edu>
2018-10-05 09:27:50 -0600

    Cleaning up CMake and adding options for -DSERIAL and -DCOMPLEX.

--- .ci-test-and-cov.sh
+++ .ci-test-and-cov.sh
@@ -3,17 +3,17 @@ cd build
 export CXX=mpicxx
 cmake ..
 make Dice
-make test-Determinants
-make test-integral
-make test-symmetry
+make unit-Determinants
+make unit-integral
+make unit-symmetry
 make test
 
 # Test coverage
 mkdir coverage
 cd coverage
-gcov ../../Dice/symmetry.cpp --object-file ../unittests/CMakeFiles/test-symmetry.dir/__/Dice/ | grep -B 1 "symmetry.cpp.gcov"
-gcov ../../Dice/Determinants.cpp --object-file ../unittests/CMakeFiles/test-Determinants.dir/__/Dice/ | grep -B 1 "Determinants.cpp.gcov"
-gcov ../../Dice/integral.cpp --object-file ../unittests/CMakeFiles/test-integral.dir/__/Dice/ | grep -B 1 "integral.cpp.gcov"
+gcov ../../Dice/symmetry.cpp --object-file ../unittests/CMakeFiles/unit-symmetry.dir/__/Dice/ | grep -B 1 "symmetry.cpp.gcov"
+gcov ../../Dice/Determinants.cpp --object-file ../unittests/CMakeFiles/unit-Determinants.dir/__/Dice/ | grep -B 1 "Determinants.cpp.gcov"
+gcov ../../Dice/integral.cpp --object-file ../unittests/CMakeFiles/unit-integral.dir/__/Dice/ | grep -B 1 "integral.cpp.gcov"
 
 # Functional Tests
 # cd ../../tests/
