dbaa8466b73225ea32f178c3ef1916cb30972e99
Nick Blunt <nicksblunt@gmail.com>
2020-08-13 16:17:02 +0100

    Replace boost hash_combine with fixed implementation
    This is because different versions of boost are giving different
    results from hash_combine, meaning that the tests fail for some
    versions. The new code will be consistent. It gives the same result
    as before for boost 1.57.0.
    
    (Tests fail becuase this hash value is used to assign determinants
    to processors. Changing this mapping changes the random walks).

--- FCIQMC/utilsFCIQMC.h
+++ FCIQMC/utilsFCIQMC.h
@@ -20,12 +20,23 @@
 #include "Determinants.h"
 #include "global.h"
 
+// This is hash_combine closely based on that used in boost. We use this
+// instead of boost::hash_combined directly, because the latter is
+// inconsistent across different versions of boost, meaning that tests fail
+// depending on the boost version used.
+template <class T>
+inline void hash_combine_proc(std::size_t& seed, const T& v)
+{
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+}
+
 // Get unique processor label for this determinant
 int getProc(const Determinant& d, int DetLenLocal) {
   std::size_t h_tot = 0;
   for (int i=0; i<DetLenLocal; i++) {
-    boost::hash_combine(h_tot, d.reprA[i]);
-    boost::hash_combine(h_tot, d.reprB[i]);
+    hash_combine_proc(h_tot, d.reprA[i]);
+    hash_combine_proc(h_tot, d.reprB[i]);
   }
   return h_tot % commsize;
 }
