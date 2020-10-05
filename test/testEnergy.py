df7c9741ee4dbb13e81f80d3a47e58d0823af21b
Nick Blunt <nicksblunt@gmail.com>
2020-10-05 21:02:32 +0100

    Replica test added for neon atom with initiator

--- test/testEnergy.py
+++ test/testEnergy.py
@@ -11,6 +11,23 @@ def check_results(eRef, eTest, tol):
         print "eRef = ", eRef
         print "eTest = ", eTest
 
+def check_results_fciqmc_replica(eRef1, eRef2, eRefVar, eTest1, eTest2, eTestVar, tol):
+
+    passed_1 = abs(eRef1 - eTest1) < tol
+    passed_2 = abs(eRef2 - eTest2) < tol
+    passed_var = abs(eRefVar - eTestVar) < tol
+
+    if (passed_1 and passed_2 and passed_var):
+        print "test passed"
+    else:
+        print "test failed"
+        print "eRef1 = ", eRef1
+        print "eTest1 = ", eTest1
+        print "eRef2 = ", eRef2
+        print "eTest2 = ", eTest2
+        print "eRefVar = ", eRefVar
+        print "eTestVar = ", eTestVar
+
 def check_results_sp(determERef, determETest, stochERef, stochETest, tol):
 
     if (abs(determERef - determETest) < tol):
@@ -31,39 +48,39 @@ if __name__ == '__main__':
 
     mc = sys.argv[1]
     tol = float(sys.argv[2])
-    
+
     if mc == 'vmc' or mc == 'gfmc' or mc == 'ci':
         fh = open(mc+'.ref', 'r')
         for line in fh:
             pass
         eRef = float(line.split()[1])
-    
+
         fh = open(mc+'.out', 'r')
         for line in fh:
             pass
         eTest = float(line.split()[1])
 
         check_results(eRef, eTest, tol)
-    
+
     elif mc == 'nevpt' or mc == 'nevpt_print' or mc == 'nevpt_read':
         fh = open(mc+'.ref', 'r')
         for line in fh:
             pass
         eRef = float(line.split()[-1])
-    
+
         fh = open(mc+'.out', 'r')
         for line in fh:
             pass
         eTest = float(line.split()[-1])
 
         check_results(eRef, eTest, tol)
-    
+
     elif mc == 'single_perturber':
         fh = open('nevpt.ref', 'r')
         for line in fh:
             pass
         determERef = float(line.split()[-1])
-    
+
         fh = open('nevpt.out', 'r')
         for line in fh:
             pass
@@ -73,28 +90,50 @@ if __name__ == '__main__':
         for line in fh:
             pass
         stochERef = float(line.split()[1])
-    
+
         fh = open('stoch_samples_0.dat', 'r')
         for line in fh:
             pass
         stochETest = float(line.split()[1])
 
         check_results_sp(determERef, determETest, stochERef, stochETest, tol)
-    
-    elif 'fciqmc' in mc:
+
+    elif mc == 'fciqmc':
         last_line = ''
         fh = open(mc+'.benchmark', 'r')
         for line in fh:
             if '#' not in line:
                 last_line = line
-        
+
         eRef = float(last_line.split()[5]) / float(last_line.split()[6])
-    
+
         fh = open(mc+'.out', 'r')
         for line in fh:
             if '#' not in line:
                 last_line = line
-    
+
         eTest = float(last_line.split()[5]) / float(last_line.split()[6])
 
         check_results(eRef, eTest, tol)
+
+    elif mc == 'fciqmc_replica':
+        last_line = ''
+        fh = open('fciqmc.benchmark', 'r')
+        for line in fh:
+            if '#' not in line:
+                last_line = line
+
+        eRef1 = float(last_line.split()[5]) / float(last_line.split()[6])
+        eRef2 = float(last_line.split()[9]) / float(last_line.split()[10])
+        eRefVar = float(last_line.split()[11]) / float(last_line.split()[12])
+
+        fh = open('fciqmc.out', 'r')
+        for line in fh:
+            if '#' not in line:
+                last_line = line
+
+        eTest1 = float(last_line.split()[5]) / float(last_line.split()[6])
+        eTest2 = float(last_line.split()[9]) / float(last_line.split()[10])
+        eTestVar = float(last_line.split()[11]) / float(last_line.split()[12])
+
+        check_results_fciqmc_replica(eRef1, eRef2, eRefVar, eTest1, eTest2, eTestVar, tol)
